e24b628d834d170481872f82f64767fe1717ba73
sivarethinam <95011857+sivarethinam@users.noreply.github.com>
2026-03-10 18:20:44 +0530

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://sri-rathna-backend.onrender.com',
         changeOrigin: true
       }
     }
